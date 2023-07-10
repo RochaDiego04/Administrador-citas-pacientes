e0bb23762421dd8d7e31b49bc68372e7644c4bd4
Diego Rocha <drochaesc@gmail.com>
2023-07-10 12:37:41 -0600

    Agregando datos de formulario a la clase Citas

--- js/app.js
+++ js/app.js
@@ -10,6 +10,46 @@ const sintomasInput  = document.querySelector('#sintomas');
 const formulario = document.querySelector('#nueva-cita');
 const listadoCitas = document.querySelector('#listado-citas');
 
+class Citas {
+    constructor() {
+        this.citas = [];
+    }
+
+    agregarCita(cita) {
+        this.citas = [...this.citas, cita];
+        console.log(this.citas);
+    }
+}
+
+class UI {
+    imprimirAlerta(mensaje, tipo) {
+        // Crear div
+        const divMensaje = document.createElement('DIV');
+        divMensaje.classList.add('text-center', 'alert', 'd-block', 'col-12');
+        
+        // Agregar clase al tipo de error
+        if(tipo === 'error') {
+            divMensaje.classList.add('alert-danger');
+        }
+        else{
+            divMensaje.classList.add('alert-success');
+        }
+
+        // Agregar texto al div
+        divMensaje.textContent = mensaje;
+
+        // Agregar al DOM
+        document.querySelector('#contenido').insertBefore(divMensaje, document.querySelector('.agregar-cita'));
+        
+        // Eliminar despues de cierto tiempo
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 2500);
+    }
+}
+
+ui = new UI();
+const administrarCitas = new Citas();
 
 // Event Listeners
 eventListeners();
@@ -20,9 +60,11 @@ function eventListeners() {
     fechaInput.addEventListener('input', datosCita);
     horaInput.addEventListener('input', datosCita);
     sintomasInput.addEventListener('input', datosCita);
+
+    formulario.addEventListener('submit', nuevaCita);
 }
 
-const CitaObj = { // Necesitan la propiedad name='' en el HTML.
+const citaObj = { // Necesitan la propiedad name='' en el HTML.
     mascota: '',
     propietario: '',
     telefono: '',
@@ -33,6 +75,40 @@ const CitaObj = { // Necesitan la propiedad name='' en el HTML.
 
 // Funciones
 function datosCita(e) { // Asignar valor del input a la propiedad del ObjCita
-    CitaObj[e.target.name] = e.target.value; 
-    console.log(CitaObj);
+    citaObj[e.target.name] = e.target.value.trim(); 
+}
+
+// Valida y agrega nueva cita a la clase Citas
+function nuevaCita(e) {
+    e.preventDefault();
+
+    // Extraer la informacion de citaObj
+    const { mascota, propietario, telefono, fecha, hora, sintomas } = citaObj;
+
+    // Validar
+    if(mascota === '' || propietario === '' || telefono === '' || fecha === '' || hora === '' || sintomas === '' ){
+        ui.imprimirAlerta('Todos los campos son obligatorios','error');
+        return;
+    }
+
+    // Generar id único
+    citaObj.id = Date.now();
+
+    // Creando una nueva cita
+    administrarCitas.agregarCita({...citaObj});
+
+    // Reiniciando el objeto
+    reiniciarObjeto();
+
+    // Limpiar campos del formulario
+    formulario.reset();
+}
+
+function reiniciarObjeto() {
+    citaObj.mascota = '';
+    citaObj.propietario = '';
+    citaObj.telefono = '';
+    citaObj.fecha = '';
+    citaObj.hora = '';
+    citaObj.sintomas = '';
 }
